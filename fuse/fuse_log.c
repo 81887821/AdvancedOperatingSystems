74dd77a9d0f3dd13ef9fbf41fde5b54ca26ad4d4
YeonGyu Jeong <81887821@hanmail.net>
2020-12-20 23:10:10 +0900

    Fixed fuse_log read size

--- fuse/fuse_log.c
+++ fuse/fuse_log.c
@@ -14,11 +14,16 @@ void fuse_append_log(void *identifier, fuse_event_t event)
 
 ssize_t fuse_logdev_read(struct file *file, char __user *buffer, size_t size, loff_t *offset) {
 	s64 num_logs = atomic64_read(&fuse_num_logs);
-	ssize_t size_to_read = min(sizeof(struct fuse_log_entry) * num_logs, *offset + size);
 
-	printk("fuse: reading logs, num_logs: %lld, size: %ld\n", num_logs, size_to_read);
-	copy_to_user(buffer, ((char *)fuse_logs) + *offset, size_to_read);
-	*offset += size_to_read;
+	if (*offset >= sizeof(struct fuse_log_entry) * num_logs) {
+		return 0;
+	} else {
+		ssize_t size_to_read = min(sizeof(struct fuse_log_entry) * num_logs - *offset, size);
 
-	return size_to_read;
+		printk("fuse: reading logs, num_logs: %lld, size: %ld\n", num_logs, size_to_read);
+		copy_to_user(buffer, ((char *)fuse_logs) + *offset, size_to_read);
+		*offset += size_to_read;
+
+		return size_to_read;
+	}
 }
